fa06a73e68e4d88e9585605cde657ab4bc61dff8
Sachin Sharma <94907192+sachinautomation25@users.noreply.github.com>
2021-11-24 12:10:32 +0530

    Create TermsAndConditions.java
    added TermsAndConditions

---
+++ src/main/java/com/qa/trcrm/pages/TermsAndConditions.java
@@ -0,0 +1,17 @@
+package com.qa.trcrm.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class TermsAndConditions {
+
+	WebDriver driver;
+
+	public TermsAndConditions(WebDriver driver) {
+		this.driver = driver;
+	}
+  public void get()
+  {
+  System.out.println("get");
+  }
+	
+}
